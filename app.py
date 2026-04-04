18b0e664c6b14c68ba10a66d3cac2328c019857b
Mahakchoudhari <mehakchoudhari786@gmail.com>
2026-04-05 00:12:23 +0530

    Add some changes

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from support_env import SupportEnv
+
+app = FastAPI()
+
+env = SupportEnv()
+
+@app.post("/reset")
+def reset(task_id: int = 0):
+    obs = env.reset(task_id)
+    return {"observation": obs}
+
+@app.post("/step")
+def step(action: dict):
+    obs, reward, done, info = env.step(action)
+    return {
+        "observation": obs,
+        "reward": reward,
+        "done": done,
+        "info": info
+    }
+
+@app.get("/state")
+def state():
+    return env.state()
